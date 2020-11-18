e98bde9bc23b67fcc6decb71b35561fd3c49a1bc
Bruno Peres <brunoperes7@hotmail.com>
2020-11-18 18:03:07 -0300

    Challenge finalized

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import {getCustomRepository, Repository} from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id:string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const deleteRepository = await transactionsRepository.findOne(id);
+    
+    if(!deleteRepository) {
+      throw new AppError("Cannot delete a Repository that does not Exists")
+    }
+
+    await transactionsRepository.remove(deleteRepository);
   }
 }
 
